c650ff053ad99ce4b41a2a35dc55d0d6890b86e5
karthikprasad-007 <karthikprasad100k@gmail.com>
2025-12-29 17:45:42 +0530

    Optimised bubbleSort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,29 +1,27 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     
     public static void main(String[] args) {
         int[] arr = {8, 2, 4, 1, 2, 5, -1, 8, 0, -7, 10, 0, 12, 6};
         bubbleSort(arr);
-        display(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void bubbleSort(int[] arr) {
         for(int i = 0; i < arr.length; ++i) {
-            for(int j = 0; j < arr.length - i - 1; ++j) {
-                if(arr[j+1] < arr[j]) {
+            boolean isSorted = true;
+            for(int j = 1; j < arr.length - i; ++j) {
+                if(arr[j] < arr[j-1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    isSorted = false;
                 }
             }
+            if(isSorted) break;
         }
     }
-
-    static void display(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
 }
